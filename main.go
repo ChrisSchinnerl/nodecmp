d31f423c9fac86e0251bde86363107ec74095621
Christopher Schinnerl <ch.schinnerl@gmail.com>
2017-12-11 17:50:33 -0500

    ping address before printing it

--- main.go
+++ main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"bufio"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
+	"io"
+	"net"
 	"os"
+	"time"
 )
 
 // some errors that might occur
 var (
-	errInvalidArgs = "provided args are invalid: "    // Indicates invalid user input
-	errInvalidMD   = "error while reading metadata: " // Indicates missing or invalid metadata
-	errDecode      = "error while decoding entries: " // Indicates a corrupted json file
+	errInvalidArgs = "provided args are invalid: "             // Indicates invalid user input
+	errInvalidMD   = "error while reading metadata: "          // Indicates missing or invalid metadata
+	errDecode      = "error while decoding entries: "          // Indicates a corrupted json file
+	errVersion     = "error while getting version from host: " // Indicates that we couldn't reach host
 )
 
 // nodeEntry is a single entry in the node.json file
@@ -23,6 +28,71 @@ type nodeEntry struct {
 // printUsage prints the usage of the nodecmp tool
 func printUsage() {
 	fmt.Print("Usage: nodecmp [path1] [path2]")
+}
+
+// readPrefix reads an object's prefix
+func readPrefix(r io.Reader) (uint64, error) {
+	prefix := make([]byte, 8)
+	if _, err := r.Read(prefix); err != nil {
+		return 0, err
+	}
+	return binary.LittleEndian.Uint64(prefix), nil
+}
+
+// writePrefix writes an object's prefix
+func writePrefix(w io.Writer, length uint64) error {
+	prefix := make([]byte, 8)
+	binary.LittleEndian.PutUint64(prefix, length)
+	if _, err := w.Write(prefix); err != nil {
+		return err
+	}
+	return nil
+}
+
+// nodeVersion gets the version of a node by pinging it
+func nodeVersion(addr string) (string, error) {
+	// Create dialer
+	dialer := &net.Dialer{
+		Timeout: 100 * time.Millisecond,
+	}
+
+	// Connect to host
+	conn, err := dialer.Dial("tcp", addr)
+	if err != nil {
+		return "", err
+	}
+
+	// Send message prefix. 8 bytes version prefix + 5 bytes version
+	if err := writePrefix(conn, uint64(13)); err != nil {
+		return "", err
+	}
+
+	// Send own version prefix
+	ownVersion := []byte("1.2.0")
+	if err := writePrefix(conn, uint64(len(ownVersion))); err != nil {
+		return "", err
+	}
+
+	// Send own version
+	if _, err := conn.Write(ownVersion); err != nil {
+		return "", err
+	}
+
+	// Receive peer version prefix
+	conn.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
+	prefix, err := readPrefix(conn)
+	if err != nil {
+		return "", err
+	}
+
+	// Receive peer version
+	conn.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
+	version := make([]byte, prefix)
+	_, err = conn.Read(version)
+	if err != nil {
+		return "", err
+	}
+	return string(version), nil
 }
 
 func main() {
@@ -88,7 +158,10 @@ func main() {
 	}
 	for _, entry := range entries2 {
 		if _, exists := entryMap[entry.Address]; exists {
-			fmt.Println(entry.Address)
+			version, err := nodeVersion(entry.Address)
+			if err == nil {
+				fmt.Printf("%v -> %v\n", entry.Address, version)
+			}
 		}
 	}
 }
