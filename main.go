409b26a5112c85a5c2d59890be46b28de9825bdf
Christopher Schinnerl <ch.schinnerl@gmail.com>
2017-12-18 18:57:05 -0500

    allow for arbitrary number of args

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ type nodeEntry struct {
 
 // printUsage prints the usage of the nodecmp tool
 func printUsage() {
-	fmt.Print("Usage: nodecmp [path1] [path2]")
+	fmt.Print("Usage: nodecmp [path1] [path2] ... [pathN]")
 }
 
 // readPrefix reads an object's prefix
@@ -95,73 +95,76 @@ func nodeVersion(addr string) (string, error) {
 	return string(version), nil
 }
 
+// loadNodes reads a nodes file and returns the entries
+func loadNodes(path string) map[string]bool {
+	// Read file
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Print(errInvalidArgs, err)
+		os.Exit(1)
+	}
+
+	// Put contents of file in buffer
+	r := bufio.NewReader(f)
+
+	// Discard the first three lines since they contain only metadata
+	for i := 0; i < 3; i++ {
+		_, _, err := r.ReadLine()
+		if err != nil {
+			fmt.Print(errInvalidMD, err)
+			os.Exit(1)
+		}
+	}
+
+	// Create decoder
+	d := json.NewDecoder(r)
+
+	// Decode entries
+	var entries []nodeEntry
+	if err := d.Decode(&entries); err != nil {
+		fmt.Print(errDecode, err)
+		os.Exit(1)
+	}
+
+	// Create a set out of the entries
+	entrySet := make(map[string]bool)
+	for _, entry := range entries {
+		entrySet[entry.Address] = entry.Outbound
+	}
+	return entrySet
+}
+
+// intersect intersects 2 maps
+func intersect(m1 map[string]bool, m2 map[string]bool) map[string]bool {
+	intersected := make(map[string]bool)
+	for key, value := range m1 {
+		if _, exists := m2[key]; exists {
+			intersected[key] = value
+		}
+	}
+	return intersected
+}
+
 func main() {
 	// Get commandline args
 	args := os.Args[1:]
 
-	// There should be 2 arguments
-	if len(args) != 2 {
+	// There should be 2 or more
+	if len(args) < 2 {
 		printUsage()
 		return
 	}
 
-	// Read files
-	f1, err1 := os.Open(args[0])
-	if err1 != nil {
-		fmt.Print(errInvalidArgs, err1)
-		os.Exit(1)
-	}
-	f2, err2 := os.Open(args[1])
-	if err2 != nil {
-		fmt.Print(errInvalidArgs, err2)
-		os.Exit(1)
+	// Pairwise intersect all entries
+	entryMap := loadNodes(args[0])
+	for _, path := range args[1:] {
+		entryMap = intersect(entryMap, loadNodes(path))
 	}
 
-	// Put contents of files in buffers
-	r1 := bufio.NewReader(f1)
-	r2 := bufio.NewReader(f2)
-
-	// Discard the first three lines since they contain only metadata
-	for i := 0; i < 3; i++ {
-		_, _, err := r1.ReadLine()
-		if err != nil {
-			fmt.Print(errInvalidMD, err)
-			os.Exit(1)
-		}
-		_, _, err = r2.ReadLine()
-		if err != nil {
-			fmt.Print(errInvalidMD, err)
-			os.Exit(1)
-		}
-	}
-
-	// Create decoders
-	d1 := json.NewDecoder(r1)
-	d2 := json.NewDecoder(r2)
-
-	// Decode entries
-	var entries1 []nodeEntry
-	var entries2 []nodeEntry
-	if err := d1.Decode(&entries1); err != nil {
-		fmt.Print(errDecode, err)
-		os.Exit(1)
-	}
-	if err := d2.Decode(&entries2); err != nil {
-		fmt.Print(errDecode, err)
-		os.Exit(1)
-	}
-
-	// Print entries that exist in both files
-	entryMap := make(map[string]struct{})
-	for _, entry := range entries1 {
-		entryMap[entry.Address] = struct{}{}
-	}
-	for _, entry := range entries2 {
-		if _, exists := entryMap[entry.Address]; exists {
-			version, err := nodeVersion(entry.Address)
-			if err == nil {
-				fmt.Printf("%v -> %v\n", entry.Address, version)
-			}
+	for address := range entryMap {
+		version, err := nodeVersion(address)
+		if err == nil {
+			fmt.Printf("%v -> %v\n", address, version)
 		}
 	}
 }
